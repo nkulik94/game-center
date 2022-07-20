758fac1d62ebfda3aec11201523fc711459d2885
Naftali Kulik <nkulik1994@gmail.com>
2022-07-20 15:59:55 -0400

    added create action for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  post "/login", to: "sessions#create"
   
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
