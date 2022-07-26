d711321df5ae559a479562ae649f7c10ca3fda42
Naftali Kulik <nkulik1994@gmail.com>
2022-07-26 15:19:04 -0400

    split user context object into multiple states

--- client/src/context/user.js
+++ client/src/context/user.js
@@ -3,23 +3,38 @@ import React, { useState } from "react";
 const UserContext = React.createContext();
 
 function UserProvider({ children }) {
-    const [user, setUser] = useState(null)
+    const [likedGames, setLikes] = useState(null)
+    const [profile, setProfile] = useState(null)
+    const [likedIds, setId] = useState({})
 
     function getMe() {
         fetch("/me")
         .then(res => {
             if (res.ok) {
                 res.json().then(user => {
-                    setUser(user)
-                    //console.log(user)
+                    setLikes(user.liked_games)
+                    const newProfile = {}
+                    Object.keys(user).map(key => {
+                        if (!Array.isArray(user[key])) {
+                            newProfile[key] = user[key]
+                        }
+                    })
+                    setProfile(newProfile)
+                    const ids = {}
+                    user.liked_games.map(game => ids[game.id] = game.id)
+                    setId(ids)
                 })
             }
         })
     }
     const currentUser = {
-        user,
-        setUser,
-        getMe
+        user: profile,
+        setUser: setProfile,
+        getMe,
+        likedGames,
+        setLikes,
+        setId,
+        likedIds
     }
 
     return <UserContext.Provider value={currentUser}>{children}</UserContext.Provider>;
