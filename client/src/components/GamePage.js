29dff7ec5d4715dd96189ce26b5ebb83ec134571
Naftali Kulik <nkulik1994@gmail.com>
2022-10-24 14:41:52 -0400

    Updated frontend to use new pagination

--- client/src/components/GamePage.js
+++ client/src/components/GamePage.js
@@ -5,26 +5,29 @@ import GameList from "./GameList";
 import Container from '@mui/material/Container';
 
 function GamePage() {
-    const params = useParams()
     const gameContext = useContext(GamesContext)
     const [searched, setSearched] = useState('')
     const [pageCount, setPageCount] = useState(0)
+    const [page, setPage] = useState(null)
+    const [activePage, setActive] = useState(1)
 
     useEffect(() => {
-        fetch(`/games?page=${params.page}`)
+        const url = page ? `/games?page=${page}` : '/games'
+        fetch(url)
             .then(r => r.json())
-            .then(({ page_count, games }) => {
+            .then(({ page_count, games, active_page }) => {
                 gameContext.setGames(games)
                 setPageCount(page_count)
+                setActive(active_page)
             })
-    }, [params.page])
+    }, [page])
     
 
     //const filteredGames = games.filter(game => game.title.toUpperCase().includes(searched.toUpperCase()))
 
     return (
         <Container sx={{marginTop: '3%'}}>
-            <GameList games={gameContext.games} searched={searched} setSearched={setSearched} params={params} totalPages={pageCount} />
+            <GameList games={gameContext.games} searched={searched} setSearched={setSearched} totalPages={pageCount} setPage={setPage} page={activePage} />
         </Container>
     )
 }
